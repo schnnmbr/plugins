311f430dcf68bb6bb73601ec1ea3d2fe36ce2ff5
schnnmbr <sachingopalakrishnan@gmail.com>
2013-11-24 16:21:08 +0530

    autocrop thumbail + auto featured image
    the featured image picked up is the first image in the post. If a feared image is specifically set, its picked up.

--- shopno2-functions/shopno2_functions.php
+++ shopno2-functions/shopno2_functions.php
@@ -97,8 +97,47 @@ function shopno2_custom_admin_bar_setting() {
 	else
 		return FALSE;
 }
+//This snippet automatically sets the featured image by fetching the first image of the post.
+//*Note – if you choose a featured image, that will be displayed instead.
+function autoset_featured() {
+          global $post;
+          $already_has_thumb = has_post_thumbnail($post->ID);
+              if (!$already_has_thumb)  {
+              $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
+                          if ($attached_image) {
+                                foreach ($attached_image as $attachment_id => $attachment) {
+                                set_post_thumbnail($post->ID, $attachment_id);
+                                }
+                           }
+                        }
+      }
+add_action('the_post', 'autoset_featured');
+add_action('save_post', 'autoset_featured');
+add_action('draft_to_publish', 'autoset_featured');
+add_action('new_to_publish', 'autoset_featured');
+add_action('pending_to_publish', 'autoset_featured');
+add_action('future_to_publish', 'autoset_featured');
+//Autocrop thumbnails fo they do not stretch and look ugly!
+// Standard Size Thumbnail
+if(false === get_option("thumbnail_crop")) {
+add_option("thumbnail_crop", "1"); }
+else {
+update_option("thumbnail_crop", "1");
+}
 
+// Medium Size Thumbnail
+if(false === get_option("medium_crop")) {
+add_option("medium_crop", "1"); }
+else {
+update_option("medium_crop", "1");
+}
 
+// Large Size Thumbnail
+if(false === get_option("large_crop")) {
+add_option("large_crop", "1"); }
+else {
+update_option("large_crop", "1");
+}
 
 
  //Uncomment Below Line To Hide Admin Bar For Logged In Users
