c567e39d9f093498152da382033ee275be7adebd
schnnmbr <sachingopalakrishnan@gmail.com>
2014-02-09 23:32:30 +0530

    Widgetized template
    Now any page can be widgetized by chooses widgetized template.
    
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/shopno2-w.php
+++ shopno2-styles/shopno2-w.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Shopno2 Widgetized FrontPage
+Plugin Name: Shopno2 Widgetized Page
 Plugin URI: http://shopno2.com
 Description: Used when you need content Before Footer
 Author: sachin nambiar
