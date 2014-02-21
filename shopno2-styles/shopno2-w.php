9cad526f6b796d1ec6b3699653952ab92b5c8c74
schnnmbr <sachingopalakrishnan@gmail.com>
2014-02-21 11:40:50 +0530

    widget area changes
    Removed Before Header
    Added Before Content
    Removed Unnecessary Wraps
    Added After Content
    
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/shopno2-w.php
+++ shopno2-styles/shopno2-w.php
@@ -8,69 +8,79 @@ Author URI: sachinnambiar.com
 Version: 0.1
 */
 //WIDGETS
-// Before Header For Widgetized Page
-	register_sidebar( array(
-		'name' => __( 'Before Header', 'shopno2' ),
-		'id' => 'bht',
-		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
-		'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '</div>',
-		'before_title' => '<h3 class="widget-title">',
-		'after_title' => '</h3>',
-	) );
 
-add_action( 'genesis_before_header', 'shopno2_sidebar_bht' );
-
-
-function shopno2_sidebar_bht() {
-if (is_page_template('shopno2_widgetized_page.php')){
-	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'bht' ) ) 
-
-{}}}
 
 // After Header For Widgetized Page
 	register_sidebar( array(
 		'name' => __( 'After Header', 'shopno2' ),
-		'id' => 'ah1t',
+		'id' => 'aht',
 		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
-		'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '</div>',
+		'before_widget' => '<div id="ah"><div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div></div>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
 
 
-add_action('genesis_after_header','shopno2_wraps_ah1t_before');
-add_action('genesis_after_header', 'shopno2_sidebar_ah1t' );
-add_action('genesis_after_header','shopno2_wraps_ah1t_after');	
+add_action('genesis_after_header', 'shopno2_sidebar_aht' );
 
-function shopno2_sidebar_ah1t() {
+
+function shopno2_sidebar_aht() {
 if (is_page_template('shopno2_widgetized_page.php')){
-	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'ah1t' ) ) {}}}
+	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'aht' ) ) {}}}
 
-//Wraps On Sidebars
-function shopno2_wraps_ah1t_before(){
-echo '<div id="ah1t">';
-}
+// Before Content For Widgetized Page
+	register_sidebar( array(
+		'name' => __( 'Before Content', 'shopno2' ),
+		'id' => 'bc',
+		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
+		'before_widget' => '<div id="bc"><div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div></div>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
 
-function shopno2_wraps_ah1t_after(){
-echo '</div>';
-}
 
+add_action('genesis_before_content', 'shopno2_sidebar_bc' );
+
+
+function shopno2_sidebar_bc() {
+if (is_page_template('shopno2_widgetized_page.php')){
+	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'bc' ) ) {}}}
+
+// After Content For Widgetized Page
+	register_sidebar( array(
+		'name' => __( 'After Content', 'shopno2' ),
+		'id' => 'ac',
+		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
+		'before_widget' => '<div id="ac"><div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div></div>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+add_action( 'genesis_after_content', 'shopno2_sidebar_ac' );
+
+
+function shopno2_sidebar_ac() {
+if (is_page_template('shopno2_widgetized_page.php')){
+	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'ac' ) ) 
+
+{}}}
 
 // Before Footer For Widgetized Page
 	register_sidebar( array(
 		'name' => __( 'Before Footer', 'shopno2' ),
 		'id' => 'bft',
 		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
-		'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
-		'after_widget' => '',
+		'before_widget' => '<div id="bf"><div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div></div>',
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
-//add_action('genesis_before_footer','shopno2_wraps_bf1t_before');
+
 add_action( 'genesis_before_footer', 'shopno2_sidebar_bft' );
-//add_action('genesis_before_footer','shopno2_wraps_bf1t_after');
+
 
 function shopno2_sidebar_bft() {
 if (is_page_template('shopno2_widgetized_page.php')){
