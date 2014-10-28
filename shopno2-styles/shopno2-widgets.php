51f9549bc11faafe9ce37b2166af532c5f045c21
schnnmbr <sachingopalakrishnan@gmail.com>
2014-10-28 20:37:16 +0530

    added before footer widget
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/shopno2-widgets.php
+++ shopno2-styles/shopno2-widgets.php
@@ -8,6 +8,23 @@ Author URI: sachinnambiar.com
 Version: 0.1
 */
 //WIDGETS
+// Before Footer For Widgetized Page
+	register_sidebar( array(
+		'name' => __( 'Before Footer', 'shopno2' ),
+		'id' => 'bf',
+		'description' => __( 'Add widgets here to appear in your sidebar.', 'shopno2' ),
+		'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</div>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+add_action( 'genesis_before_footer', 'shopno2_sidebar_bf' );
+
+
+function shopno2_sidebar_bf() {
+
+	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'bf' ) ) 
 
 // After entry on Single Page
 	register_sidebar( array(
@@ -25,3 +42,4 @@ add_action( 'genesis_after_entry', 'shopno2_sidebar_ap' );
 function shopno2_sidebar_ap() {
 if (is_singular()){
 	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'ap' ) ) {}}}
+
