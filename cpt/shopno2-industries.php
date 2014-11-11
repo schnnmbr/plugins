805ab0c7b393496c7d736aa4e6d38c0aa3be524a
schnnmbr <sachingopalakrishnan@gmail.com>
2014-11-11 17:30:28 +0530

    added s2 to names to prevent conflict with repository ..
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- cpt/shopno2-industries.php
+++ cpt/shopno2-industries.php
@@ -1,7 +1,7 @@
 <?php 
 
 /*
-Plugin Name:12-Industries
+Plugin Name:12-s2Industries
 Plugin URI: http://shopno2.com
 Description: Industries Custom Post Type For Use Through Out The Site
 Use Industries to add and display industries to your site.
