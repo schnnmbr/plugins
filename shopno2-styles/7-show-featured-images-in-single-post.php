b976949f11c78084367acabfb36320fd60a803ee
schnnmbr <sachingopalakrishnan@gmail.com>
2014-11-11 19:44:59 +0530

    trying to make the featured image work for cpt
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/7-show-featured-images-in-single-post.php
+++ shopno2-styles/7-show-featured-images-in-single-post.php
@@ -13,7 +13,7 @@ add_action( 'genesis_after_header', 's2_show_featured_image_single_posts', 9 );
  * Display Featured Image floated to the right in single Posts.
  */
 function s2_show_featured_image_single_posts() {
-	if ( ! is_singular() ) {
+	if ( ! is_singular('post','page','customer','industry','product') ) {
 		return;
 	}
  
