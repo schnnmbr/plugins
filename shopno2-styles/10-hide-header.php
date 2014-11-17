f46626e7efd95b466e3332c3e289f6318e208241
schnnmbr <sachingopalakrishnan@gmail.com>
2014-11-18 01:37:42 +0530

    modified description
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/10-hide-header.php
+++ shopno2-styles/10-hide-header.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: 10 - Hide Header On Single Posts
+Plugin Name: 10 - Hide Header except on front page
 Plugin URI: http://shopno2.com
 Description: Hides Header
 Author: shopno2.com
