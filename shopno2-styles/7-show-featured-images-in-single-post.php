7b6962172d9c29688b4975b1c5326a7b3aa36140
schnnmbr <sachingopalakrishnan@gmail.com>
2014-11-18 21:43:23 +0530

    featured image position separated for pages and posts
    Signed-off-by: schnnmbr <sachingopalakrishnan@gmail.com>

--- shopno2-styles/7-show-featured-images-in-single-post.php
+++ shopno2-styles/7-show-featured-images-in-single-post.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: 7 - Show Featured Image In Single Posts
+Plugin Name: 7 - Location Of Featured Image In Single Posts & Pages
 Plugin URI: http://shopno2.com
 Description: Customise your link color
 Author: sachin nambiar
@@ -8,12 +8,13 @@ Author URI: sachinnambiar.com
 Version: 0.1
 */
 
-add_action( 'genesis_before_content_sidebar_wrap', 's2_show_featured_image_single_posts', 9 );
+add_action( 'genesis_after_header', 's2_show_featured_image_single_page', 9 );
+add_action( 'genesis_entry_header', 's2_show_featured_image_single_posts', 9 );
 /**
  * Display Featured Image floated to the right in single Posts.
  */
-function s2_show_featured_image_single_posts() {
-	if ( ! is_singular('') ) {
+function s2_show_featured_image_single_page() {
+	if ( !is_page('') ) {
 		return;
 	}
  
@@ -21,6 +22,21 @@ function s2_show_featured_image_single_posts() {
 		'size' => '',
 		'attr' => array(
 			'class' => 'aligncenter',
+		),
+	);
+ 
+	genesis_image( $image_args );
+}
+
+function s2_show_featured_image_single_posts() {
+	if ( !is_single('') ) {
+		return;
+	}
+ 
+	$image_args = array(
+		'size' => '',
+		'attr' => array(
+			'class' => 'alignnone',
 		),
 	);
  
